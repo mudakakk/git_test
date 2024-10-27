cf4198424fac919065cda0c6520a99320c13be22
mudakakk <161918354+mudakakk@users.noreply.github.com>
2024-10-27 19:19:27 +0000

    finisged the calc exercise ^-^

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,4 @@
-const removeFromArray = function(arr, int1, int2, int3, int4, int5, int6) {
-    const nums = (Array.from(arguments)).slice(1);
+const removeFromArray = function(arr, ...nums) {
     for (const num of nums){
         while (arr.indexOf(num) !== -1){
             arr.splice(arr.indexOf(num), 1)
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(summand1, summand2) {
+	return summand1 + summand2;
 };
 
-const subtract = function() {
-	
+const subtract = function(minuend, subtrahend) {
+	return minuend - subtrahend;
 };
 
-const sum = function() {
-	
+const sum = function(...nums) {
+	return nums[0] ? nums.reduce((accumulator, current) => accumulator + current) : 0;
 };
 
-const multiply = function() {
-
+const multiply = function(multiplier, ...multiplicands) {
+  for (const multiplicand of multiplicands) {
+    multiplier *= multiplicand;
+  }
+  return multiplier;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return Math.pow(base, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorial = 1;
+  for (i = 1; i <= num; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
