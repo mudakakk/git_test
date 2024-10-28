ce5765b3c4a4998106405ca88dac26093ffe0a71
mudakakk <161918354+mudakakk@users.noreply.github.com>
2024-10-28 15:20:01 +0000

    completed the palindrome exercise !!!!!! :3

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,7 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const regex = /\w/i;
+    const filteredArray = str.toLowerCase().split('').filter((char) => regex.test(char));
+    return (filteredArray.join('') === filteredArray.reverse().join(''));
 
 };
 
